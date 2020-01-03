8ac2bd87d32240257d4187e024dbc11cb3f7572a
Mokytex <vanzha.alexander@gmail.com>
2020-01-03 11:35:52 +0300

    5

--- bidentCore.py
+++ bidentCore.py
@@ -3,6 +3,7 @@ import os
 import time
 import multiprocessing
 import argparse
+import ast
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -17,63 +18,62 @@ class Launcher(QApplication):
         self.setApplicationName('DotaBident')
         self.exePath = self.arguments()[0]
         self.exeDir = os.path.dirname(self.exePath)
+        self.isSecondInstance = False
+        self.isSocketConnected = False
+        self.socket = QLocalSocket()
+        
         self.processArguments()
-        # self.singleInstanceChecked()
+        self.singleInstanceChecked()
         
-   
         self.server = QLocalServer()
-        
-        
         self.server.newConnection.connect(self.newInstanceConnected)
-   
         self.server.listen("Bident Server")
-        
-        
-     
+    
     def processArguments(self):
         self.argParser = argparse.ArgumentParser(description='Bident Parser')
-        self.argParser.add_argument('-debug',
-                                    action="store_true",
-                                    default=False)
+        self.argParser.add_argument('-debug', action="store_true", default=False)
         self.argParser.add_argument('-token', action="store")
         self.argDict = vars(self.argParser.parse_args())
-      
+    
     def singleInstanceChecked(self):
-        self.socket.connectToServer("Bident Server")
-        time.sleep(1)
-        self.socket.connected()
-        
+        self.socket.connectToServer("Bident Server", QIODevice.ReadWrite)
+        self.socket.readyRead.connect(self.socketConnected)
+        self.socket.waitForReadyRead(500)
+        if self.isSocketConnected:
+            self.socketWritten(self.argDict)
+            self.socket.disconnectFromServer()
+            self.isSecondInstance = True
+        else:
+            pass
+    
     def socketConnected(self):
-        pass
-    
-    def socketError(self):
-        pass
-    
-    def socketDisconnected(self):
-        pass
-    
-    def socketWritten(self, bytes):
-        pass
+        response = self.socket.readAll().data().decode('utf-8')
+        if response == 'Connected':
+            self.isSocketConnected = True
+        else:
+            self.socket.disconnectFromServer()
+            raise ConnectionError
+        
+    def socketWritten(self, data):
+        self.socket.write(bytes(str(data), encoding='utf-8'))
     
     def socketReading(self):
-        data = self.socket.readAll()
-        print(data)
+        self.data = ast.literal_eval(self.socket.readAll().data().decode('utf-8'))
+        if self.data['token'] is not None:
+            print(self.data['token'])
     
     def newInstanceConnected(self):
-        print("newInstanceConnected")
         self.socket = self.server.nextPendingConnection()
         self.socket.write(b'Connected')
         self.socket.readyRead.connect(self.socketReading)
-        
-        
+
     def launchApplication(self):
         pass
-    
-        
 
 
 if __name__ == '__main__':
-
     app = Launcher(sys.argv)
-    app.exec_()
-    
+    if not app.isSecondInstance:
+        app.exec_()
+    else:
+        pass
--- bidentHook.py
+++ bidentHook.py
@@ -1,12 +1,9 @@
 import sys
 import os
 import time
-import math
-import subprocess
-from Pyro4 import *
-from Pyro4.core import *
-from Pyro4.naming import *
-
+import multiprocessing
+import argparse
+import ast
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -17,19 +14,66 @@ class Launcher(QApplication):
     
     def __init__(self, args):
         super(Launcher, self).__init__(args)
-
+        
+        self.setApplicationName('DotaBident')
+        self.exePath = self.arguments()[0]
+        self.exeDir = os.path.dirname(self.exePath)
+        self.isSecondInstance = False
+        self.isSocketConnected = False
         self.socket = QLocalSocket()
+        
+        self.processArguments()
+        self.singleInstanceChecked()
+        
+        self.server = QLocalServer()
+        self.server.newConnection.connect(self.newInstanceConnected)
+        self.server.listen("Bident Server")
+    
+    def processArguments(self):
+        self.argParser = argparse.ArgumentParser(description='Bident Parser')
+        self.argParser.add_argument('-debug', action="store_true", default=False)
+        self.argParser.add_argument('-token', action="store")
+        self.argDict = vars(self.argParser.parse_args())
+    
+    def singleInstanceChecked(self):
         self.socket.connectToServer("Bident Server", QIODevice.ReadWrite)
         self.socket.readyRead.connect(self.socketConnected)
-
+        self.socket.waitForReadyRead(500)
+        if self.isSocketConnected:
+            self.socketWritten(self.argDict)
+            self.socket.disconnectFromServer()
+            self.isSecondInstance = True
+        else:
+            pass
+    
     def socketConnected(self):
         response = self.socket.readAll().data().decode('utf-8')
         if response == 'Connected':
-            print('Connected')
+            self.isSocketConnected = True
         else:
+            self.socket.disconnectFromServer()
             raise ConnectionError
+    
+    def socketWritten(self, data):
+        self.socket.write(bytes(str(data), encoding='utf-8'))
+    
+    def socketReading(self):
+        self.data = ast.literal_eval(self.socket.readAll().data().decode('utf-8'))
+        if self.data['token'] is not None:
+            print(self.data['token'])
+    
+    def newInstanceConnected(self):
+        self.socket = self.server.nextPendingConnection()
+        self.socket.write(b'Connected')
+        self.socket.readyRead.connect(self.socketReading)
+    
+    def launchApplication(self):
+        pass
+
 
 if __name__ == '__main__':
-    app = Launcher([])
-    # socket.disconnectFromServer()
-    app.exec_()
+    app = Launcher(sys.argv)
+    if not app.isSecondInstance:
+        app.exec_()
+    else:
+        pass

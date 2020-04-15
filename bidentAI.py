f0fa7ebeb51c557ff2b935429f9f0f46a7af83e3
Mokytex <vanzha.alexander@gmail.com>
2020-04-16 02:48:45 +0300

    19

--- bidentAI.py
+++ bidentAI.py
@@ -10,7 +10,7 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\t
 
 class RWDError(Exception):
     
-    def __init__(self, name: str, *args):
+    def __init__(self, name: str, *args, **kwargs):
         self.names = {
             'Already exist': 1,
             'Doesn\'t exist': 2,
@@ -20,10 +20,14 @@ class RWDError(Exception):
         self.name = name
         self.code = self.names[name]
         self.args = list(args)
-    
+        self.kwargs = kwargs
+
     def __str__(self):
-        tmp = ', '.join(map(str, self.args))
-        return f'{self.name}: {tmp}'
+        tmp = '.'.join(map(str, self.args))
+        if self.kwargs['hint']:
+            return f'{self.name}: {tmp} \n{self.kwargs["hint"]}'
+        else:
+            return f'{self.name}: {tmp}'
 
 
 class Area:
@@ -35,7 +39,12 @@ class Area:
                 'imgSize': (0, 0),
                 'topLeft': (0, 0),
                 'bottomRight': (0, 0),
-                'mod': (0, 0)
+                'mod': ((0, 0), (0, 0))
+            },
+            'test': {
+                'imgSize': (1920, 1080),
+                'topLeft': (144, 36),
+                'bottomRight': (720, 480)
             }
         }
     
@@ -62,20 +71,24 @@ class Area:
         except:
             raise RWDError('Doesn\'t exist', name)
     
-    def setTemplateProperties(self, name: str, mod: tuple = (0, 0), rx: int = None, ry: int = None):
+    def setTemplateProperties(self, name: str, mod: list = [[0, 0], [0, 0]], rx: int = None, ry: int = None):
         """
-        :param mod: (x, y)
-        0: absolute positioning
-        1: relative positioning
-        2: relative to rPoint
+        :param mod: (x=(a, b), y=(a, b)
+        a:
+            0: absolute positioning
+            1: relative positioning
+        b:
+            0: to top\left side
+            1: to bottom\right side
+            2: to rPoint
         :param rx: X-coordinate of rPoint
         :param ry: Y-coordinate of rPoint
         """
         r = (rx, ry)
         b = list(map(
             operator.or_,
-            list(map(lambda x: (x < 2), mod)),
-            list(map(lambda x, y: (x == 2 and bool(y)), mod, r))
+            list(map(lambda x: (x[1] != 2), mod)),
+            list(map(lambda x, y: (x[1] == 2 and y is not None), mod, r))
         ))
         if not b[0]:
             raise RWDError('Additional argument required', 'rx')
@@ -83,9 +96,9 @@ class Area:
             raise RWDError('Additional argument required', 'ry')
         if type(name) is not str:
             raise RWDError('Incorrect argument type', 'name', type(name))
-
+        
         self.templates[name].update({'mod': mod, 'rx': rx, 'ry': ry})
-
+    
     def getBox(self, imgSize: tuple, templateName: str) -> tuple:
         """
         Returns box for given imgSize and templateName
@@ -93,12 +106,22 @@ class Area:
         :param templateName: Given template
         :return box: (top-left point: (x, y), box size: (x, y))
         """
-        pass
+        try:
+            self.templates[templateName]
+        except:
+            raise RWDError('Doesn\'t exist', templateName)
+        try:
+            self.templates[templateName]['mod']
+        except:
+            raise RWDError('Doesn\'t exist', templateName, 'mod',
+                           hint='Properties haven\'t been set. Run .setTemplatePropreties to set them')
+        
+        box = [[], []]
 
 
 A = Area()
 try:
     # A.setTemplateProperties('sdfg', (2, 0))
-    A.delTemplate('fgh')
+    A.getBox((), 'test')
 except Exception as e:
     print(e)

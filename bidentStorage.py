7a0c3e7a7c8d23112a0c30737de6247b6d617e49
Mokytex <vanzha.alexander@gmail.com>
2020-04-16 23:44:10 +0300

    21

--- bidentStorage.py
+++ bidentStorage.py
@@ -1,13 +1,8 @@
-import sys
-import os
+import re
+import sqlite3
 import time
-import multiprocessing
-import argparse
-import ast
+
 from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtNetwork import *
 
 
 class LocalStorage(QStorageInfo):
@@ -16,29 +11,35 @@ class LocalStorage(QStorageInfo):
         super(LocalStorage, self).__init__(parent)
         
         self.conn = sqlite3.connect('data.db')
-        self.c = self.conn.cursor()
         self.initDB()
-        
+    
     def initDB(self):
-        # self.c.execute("""
-        # CREATE TABLE IF NOT EXISTS `Settings` ()
-        # """)
-        self.c.execute("""
-        CREATE TABLE IF NOT EXISTS `Accounts` (
-        `idAccount` INT NOT NULL,
-        `Name` VARCHAR(64) NOT NULL,
-        `Avatar` BLOB NULL,
-        `Service` VARCHAR(64) NOT NULL,
-        `Cookie` JSON NULL,
-        `Date Added` TIMESTAMP NOT NULL,
-        PRIMARY KEY (`idAccount`))
+        c = self.conn.cursor()
+        c.execute("""
+        CREATE TABLE IF NOT EXISTS `Dota2 D` (
+        `Time` INT NOT NULL,
+        `MMR` INT,
+        `BScore` INT,
+        `Rank` INT,
+        `Percent` INT,
+        PRIMARY KEY (`Time`))
         """)
-        self.c.execute("""
-        CREATE TABLE IF NOT EXISTS `Services` (
-        `idService` INT NOT NULL,
-        `Name` VARCHAR(64) NULL,
-        PRIMARY KEY (`idService`))
-        """)
+    
+    def write(self, name: str, data: int):
+        c = self.conn.cursor()
+        if name == '' or 'mmr':
+            sqlQ = '''INSERT INTO "Dota2 D"(Time, MMR) VALUES (?, ?)'''
+        elif name == 'b':
+            sqlQ = '''INSERT INTO "Dota2 D"(Time, BScore) VALUES (?, ?)'''
+        elif name == 'r':
+            sqlQ = '''INSERT INTO "Dota2 D"(Time, Rank) VALUES (?, ?)'''
+        elif name == 'p':
+            sqlQ = '''INSERT INTO "Dota2 D"(Time, Percent) VALUES (?, ?)'''
+        else:
+            print('Available names: /mmr, b, r, p')
+        c.execute(sqlQ, [int(time.time()), data])
+        self.conn.commit()
+        print('Done')
     
     def setSetting(self, id, key):
         pass
@@ -70,8 +71,15 @@ class LocalStorage(QStorageInfo):
     def getServiceData(self, name):
         pass
 
+
 if __name__ == '__main__':
     db = LocalStorage()
-
-        
-    
+    exe = True
+    while exe:
+        tmp = input('Enter command\data: ')
+        t = r'(e)|(|b|r|p)(\d{2,4})'
+        r = re.fullmatch(t, tmp).groups()
+        if r[0] == 'e':
+            exe = False
+            break
+        db.write(r[1], int(r[2]))
